2443727e2135b34c4e9d0db79bd934cb90af27cf
drspahr <spahrdg@gmail.com>
2023-05-13 23:50:56 -0500

    W4 Assignment

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,15 @@
+document.querySelector(".year").textContent = new Date().getFullYear();
+document.querySelector(".moddate").textContent = document.lastModified;
+
+function toggleMenu() {
+    document.getElementById("mainNav").classList.toggle("open");
+    document.getElementById("menuBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('menuBtn')
+x.onclick = toggleMenu;
+
+const dateFld = document.querySelector("time");
+const today = new Date();
+const fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(today);
+dateFld.textContent = fullDate;
