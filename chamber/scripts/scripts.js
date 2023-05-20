1e7e61652013ab927f11b73c1fa0198fea2d2edb
drspahr <spahrdg@gmail.com>
2023-05-19 19:01:01 -0500

    Chambers test

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,10 +6,20 @@ function toggleMenu() {
     document.getElementById("menuBtn").classList.toggle("open");
 }
 
-const x = document.getElementById('menuBtn')
+const x = document.getElementById("menuBtn")
 x.onclick = toggleMenu;
 
 const dateFld = document.querySelector("time");
 const today = new Date();
 const fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(today);
 dateFld.textContent = fullDate;
+
+const banDay = new Date().getDay();
+if (banDay === 1 || banDay === 2) {
+    displayChange();
+}
+
+function displayChange () {
+    document.querySelector(".banner").style.display="block";
+}
+
