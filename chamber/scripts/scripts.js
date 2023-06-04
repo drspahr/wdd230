d033eac9275d4edb01dcc887e1674c18922b3592
drspahr <spahrdg@gmail.com>
2023-06-04 00:21:45 -0500

    Discover page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,4 @@
+// Menu button control
 document.querySelector(".year").textContent = new Date().getFullYear();
 document.querySelector(".moddate").textContent = document.lastModified;
 
@@ -9,6 +10,7 @@ function toggleMenu() {
 const x = document.getElementById("menuBtn")
 x.onclick = toggleMenu;
 
+// Banner control
 const dateFld = document.querySelector("time");
 const today = new Date();
 const fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(today);
@@ -27,4 +29,35 @@ const clsBan = document.querySelector(".banBtn");
 clsBan.addEventListener('click', () => {
     document.querySelector(".banner").style.display="none";
 });
+
+// Visit counter
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const visitDisplay = document.querySelector(".visits");
+
+if (numVisits != 0) {
+    visitDisplay.textContent = numVisits;
+} else {
+    visitDisplay.textContent = `This is your first visit. Welcome!`;
+}
+
+numVisits ++;
+localStorage.setItem("numVisits-ls", numVisits);
+let pDay = Date.now();
+localStorage.setItem("pDay-ls", pDay);
+
+// Days since last visit
+const msToDays = 84600000;
+const vtoday = Date.now();
+const dayCount = document.querySelector(".lvisit");
+
+pDay = Number(window.localStorage.getItem("pDay-ls"));
+let dayDiff = (vtoday - pDay) / msToDays;
+
+if (dayDiff < 1) {
+    dayCount.textContent = 0;
+} else {
+    dayCount.textContent = `${dayDiff.toFixed(0)} days`;
+}
+
+
 
