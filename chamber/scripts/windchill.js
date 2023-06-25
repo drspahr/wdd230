981c81db02a102d01cf1eed9cfb6400695461895
drspahr <spahrdg@gmail.com>
2023-06-24 23:31:39 -0500

    Week 10 - Weather API

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,6 +2,8 @@ const currentTemp = document.querySelector("#temp");
 const weatherIcon = document.querySelector("#weathericon");
 const windSpeed = document.querySelector("#ws");
 const des = document.querySelector("#descr");
+let temp = 0;
+let ws = 0;
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Hutchinson&units=imperial&appid=6018f46e2305c3ccbf39479a93a557cc";
 
@@ -25,15 +27,17 @@ weatherFetch();
 function displayWeather(wData) {
     currentTemp.textContent = wData.main.temp.toFixed(1);
     windSpeed.textContent = wData.wind.speed.toFixed(1);
+    temp = wData.main.temp;
+    ws = wData.wind.speed;
 
-    const iconSrc = `https://openweathermap.org/img/w/${wData.weather[0].icon}.png`;
+    const iconSrc = `https://openweathermap.org/img/wn/${wData.weather[0].icon}@2x.png`;
     const descr = wData.weather[0].description;
 
     weatherIcon.setAttribute("src", iconSrc);
     weatherIcon.setAttribute("alt", descr);
     des.textContent = descr;
-
 }
+
 // Calculate Windchill
 
 // f = 35.74 + 0.6215(t) - 35.75(s^0.16) + 0.4275(t)(s^0.16)
@@ -41,8 +45,6 @@ function displayWeather(wData) {
 // t = average air temperature in Fahrenheit
 // s = wind speed in miles per hour
 
-const temp = document.getElementById("temp").innerHTML;
-const ws = document.getElementById("ws").innerHTML;
 let wc = 0;
 
 if (temp <= 50 && ws > 3.0) {
